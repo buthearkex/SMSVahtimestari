b34b1224c7762ffbfeec254910e0e8625ca93a5a
Aarn <pelto.karvanen@gmail.com>
2013-11-14 13:29:16 +0200

    Korjatut commandsit utf-8

--- SMSVahtimestari/src/smsVahtimestari/Commands.scala
+++ SMSVahtimestari/src/smsVahtimestari/Commands.scala
@@ -5,7 +5,7 @@ object Commands extends Enumeration {
 	val Sauna = "sauna"
 	val Oven = "uuni"
 	val Car = "auto"
-	val AC = "l‰mmitys"
-	val Electricity = "s‰hkˆ"
+	val AC = "l√§mmitys"
+	val Electricity = "s√§hk√∂"
 	val DoorGuard = "ovivahti"
 }
