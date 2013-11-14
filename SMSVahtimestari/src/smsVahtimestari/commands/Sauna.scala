73bef4eb3e767a9d89fd7a6368e7b11c5d28478e
Aarn <pelto.karvanen@gmail.com>
2013-11-14 14:42:54 +0200

    Saunan pohja

--- SMSVahtimestari/src/smsVahtimestari/commands/Sauna.scala
+++ SMSVahtimestari/src/smsVahtimestari/commands/Sauna.scala
@@ -4,4 +4,13 @@ import smsVahtimestari.TemperatureTrait
 
 object Sauna extends TemperatureTrait {
 	
+	def setTemperature(temperature:Int):String = {
+		//tutkitaan arvo
+		if(temperature > 120 && temperature < 20) {
+			"Luku ei kelpaa. Anna lämpötila 120-20 väliltä"
+		}
+		else{
+			"Kelpaa"
+		}
+	}
 }
