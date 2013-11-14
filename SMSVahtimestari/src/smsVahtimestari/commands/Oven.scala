2892c6e1845f5e3579267e86f9783d82aee7abae
Aarn <pelto.karvanen@gmail.com>
2013-11-14 14:53:27 +0200

    Oven pohjaa

--- SMSVahtimestari/src/smsVahtimestari/commands/Oven.scala
+++ SMSVahtimestari/src/smsVahtimestari/commands/Oven.scala
@@ -3,5 +3,18 @@ package smsVahtimestari.commands
 import smsVahtimestari.TemperatureTrait
 
 object Oven extends TemperatureTrait {
+	def setTemperature(temperature:Int):String = {
+		//tutkitaan arvo
+		if(temperature > 300 && temperature < 20) {
+			"Luku ei kelpaa. Anna lämpötila 300-20 väliltä"
+		}
+		else{
+			"Kelpaa, lämpenee "+ temperature.toString
+		}
+	}
+
+	def getCurrentTemperature(): Int = {
+		25
+	}
 
 }
