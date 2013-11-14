288fa50a795fd120fac3203dfdc9daa46c284f03
Butterkex <mikko.s.honkanen@aalto.fi>
2013-11-14 14:39:07 +0200

    Update AirConditioning.scala

--- SMSVahtimestari/src/smsVahtimestari/commands/AirConditioning.scala
+++ SMSVahtimestari/src/smsVahtimestari/commands/AirConditioning.scala
@@ -4,4 +4,20 @@ import smsVahtimestari.OnOffTrait
 
 object AirConditioning extends OnOffTrait {
 
+  private  var isOn = false
+
+
+  def status():String = {
+    
+  }
+  
+  
+  def turnOnOff(status:Boolean):String = {
+    if(this.status)
+  }
+
+  private def switch() = {
+    
+  }
+
 }
