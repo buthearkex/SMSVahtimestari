4053356bbada7a05aae1772df55c591bdbbcdee4
Aarn <pelto.karvanen@gmail.com>
2013-11-14 13:33:31 +0200

    CommandInterpreter pohja

--- SMSVahtimestari/src/smsVahtimestari/CommandInterpreter.scala
+++ SMSVahtimestari/src/smsVahtimestari/CommandInterpreter.scala
@@ -1,5 +1,15 @@
 package smsVahtimestari
+import Commands._
 
 object CommandInterpreter {
-
+	var topic = None
+	
+	def interpret (msg: String): String = {
+		msg match{
+			case SAUNA => {
+				topic = SAUNA
+				Sauna.status()
+			}
+		}
+	}
 }
