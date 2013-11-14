62c88e8e5050430cd19199cacf2cc7527d69d3d1
Aarn <pelto.karvanen@gmail.com>
2013-11-14 16:06:20 +0200

    Time-objekti jolla getMinutesTo("1245") Int =, Ja joka ei vielä toimi

---
+++ SMSVahtimestari/src/smsVahtimestari/commands/Time.scala
@@ -0,0 +1,31 @@
+import java.util.Calendar
+import java.text.SimpleDateFormat
+
+object Time{
+	def getMinutesTo(futureTime: String): Int = {
+		val currentTime = getCurrentTime
+		val currentTimeChars = currentTime.split("")
+		val futureTimeChars = futureTime.split("")
+		
+		var mins = 0
+		val currentMins = (currentTimeChars(2) + currentTimeChars(3)).toInt
+		val futureMins = (futureTimeChars(2) + futureTimeChars(3)).toInt
+		mins = futureMins - currentMins 
+
+		var hours = 0
+		val currentHours = (currentTimeChars(0) + currentTimeChars(1)).toInt
+		val futureHours = (futureTimeChars(0) + futureTimeChars(1)).toInt
+		hours = futureHours - currentHours
+
+		mins += hours*60
+		
+		return mins
+	}
+  
+	def getCurrentTime: String = {
+		val today = Calendar.getInstance().getTime()
+		val timeFormat = new SimpleDateFormat("HHmm")
+		val time = timeFormat.format(today)
+		return "" + time
+	}
+}
