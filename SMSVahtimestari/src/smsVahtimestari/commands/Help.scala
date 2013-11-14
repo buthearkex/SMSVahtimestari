4187c8d2a6a61b3f2dd21e78d7486ace0169a853
Aarn <pelto.karvanen@gmail.com>
2013-11-14 14:53:07 +0200

    Helpin pohja

--- SMSVahtimestari/src/smsVahtimestari/commands/Help.scala
+++ SMSVahtimestari/src/smsVahtimestari/commands/Help.scala
@@ -3,5 +3,17 @@ package smsVahtimestari.commands
 import smsVahtimestari.StatusTrait
 
 object Help extends StatusTrait {
-
+	def status():String = {
+		"Tässäpä sinulle ohjeet: \n"+
+		"Sauna \n" +
+		" \n" +
+		" \n" +
+		" \n" +
+		" \n" +
+		" \n" +
+		" \n" +
+		" \n" +
+		" \n" +
+		" \n" +
+	}
 }
