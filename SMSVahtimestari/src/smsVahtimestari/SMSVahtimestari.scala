8f797698e298d136ca3878ffdd1a6b7bb4dbf909
Lauri Lavanti <laurilavanti@gmail.com>
2013-11-14 13:08:12 +0200

    Ääkköset korjattu

--- SMSVahtimestari/src/smsVahtimestari/SMSVahtimestari.scala
+++ SMSVahtimestari/src/smsVahtimestari/SMSVahtimestari.scala
@@ -11,5 +11,5 @@ object SMSVahtimestari extends App{
 	val receiver: SMSReceiver = new SMSReceiver(handleMessage)
 	val sender: SMSSender = new SMSSender
 	
-	println("SMSVahtimestari on p‰‰ll‰. L‰het‰ k‰sky " + Help + " mik‰li tarvitset apua k‰ytˆss‰.")
+	println("SMSVahtimestari on p√§√§ll√§. L√§het√§ k√§sky " + Help + " mik√§li tarvitset apua k√§yt√§ss√§.")
 }
