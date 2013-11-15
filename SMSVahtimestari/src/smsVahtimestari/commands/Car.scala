c2862be2b42687e19f4561376e06307bf484888c
Aarn <pelto.karvanen@gmail.com>
2013-11-15 12:26:51 +0200

    Car (autonlämmityksen) asdf-versio valmis

--- SMSVahtimestari/src/smsVahtimestari/commands/Car.scala
+++ SMSVahtimestari/src/smsVahtimestari/commands/Car.scala
@@ -3,12 +3,26 @@ package smsVahtimestari.commands
 import smsVahtimestari.TimerTrait
 
 object Car extends TimerTrait {
+	val heatingTimeMin = 15
+	val clock = new Time()//MITES VIDUUS TÄS SCALAAS TEHDÄÄ OLIOIIT???
+	var isOn = false	
 
-	def status(): String = ???
+	def turnOnOff(status: Boolean): String = {
+		if(isOn && status) "Auton lämmitys on jo päällä" 
+		else if(isOn) "Auton lämmitys sammutetaan" isOn = false 
+		else if(status)"Auton lämmitys lämmitetään" isOn = true
+		else "Auton lämmitys ei ollut päällä" 
+	}
 
-	def turnOnOff(status: Boolean): String = ???
+	def status(): String = {
+		if(isOn) "Auton lämmitys päällä"
+		else "Auton lämmitys ei päällä"
+	}
 
-	def setTimer(time: Int): String = ???
+	def setTimer(time: Int): String = {
+		if(clock.getMinutesTo(time) > heatingTimeMin)"Liian vähän aikaa, aloitetaan lämmitys nyt, valmista on... "
+		else "Kelpaa, lämpenee valmiiksi kello " + time.toString
+	}
 	
 	def toString = "auto"
 }
