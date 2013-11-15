41944e0cd0a2b028b996b775ae997784d61a3117
Aarn <pelto.karvanen@gmail.com>
2013-11-15 12:12:41 +0200

    Electricity luokka asdf kirjoitettu

--- SMSVahtimestari/src/smsVahtimestari/commands/Electricity.scala
+++ SMSVahtimestari/src/smsVahtimestari/commands/Electricity.scala
@@ -3,8 +3,10 @@ package smsVahtimestari.commands
 import smsVahtimestari.StatusTrait
 
 object Electricity extends StatusTrait {
+	val rnd = new Random()
+	val elConsumption = rnd.nextInt(200, 10000)
 
-	def status(): String = ???
+	def status(): String = "Sähköä kuluu nyt "+ elConsumption.toString +"Wattia"
 
 	def toString = "sähkö"
 }
