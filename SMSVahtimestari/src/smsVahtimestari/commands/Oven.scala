ddbb6231d11d7826c0ddb85b5c6d61721bffe29a
Aarn <pelto.karvanen@gmail.com>
2013-11-15 12:13:26 +0200

    Oven asdf valmis

--- SMSVahtimestari/src/smsVahtimestari/commands/Oven.scala
+++ SMSVahtimestari/src/smsVahtimestari/commands/Oven.scala
@@ -4,6 +4,10 @@ import smsVahtimestari.TemperatureTrait
 
 object Oven extends TemperatureTrait {
 	def setTemperature(temperature: Int): String = {
+		val heatingTimeMin = 15
+		val currtentTemperature = 25
+		val clock = new Time()//MITES VIDUUS TÄS SCALAAS TEHDÄÄ OLIOIIT???
+		var isOn = false
 		//tutkitaan arvo
 		if (temperature > 300 && temperature < 20) {
 			"Luku ei kelpaa. Anna lämpötila 300-20 väliltä"
@@ -16,11 +20,22 @@ object Oven extends TemperatureTrait {
 		25
 	}
 
-	def turnOnOff(status: Boolean): String = ???
+	def turnOnOff(status: Boolean): String = {
+		if(isOn && status) "Uuni on jo päällä" 
+		else if(isOn) "Uuni sammutetaan" isOn = false 
+		else if(status)"Uuni lämmitetään" isOn = true
+		else "Uuni ei ollut päällä" 
+	}
 
-	def status(): String = ???
+	def status(): String = {
+		if(isOn) "Uuni on lämpenemässä ja sen lämpö on nyt " + currtentTemperature.toString
+		else "Uuni ei ole päällä ja sen lämpö on nyt " + currtentTemperature.toString
+	}
 
-	def setTimer(time: Int): String = ???
+	def setTimer(time: Int): String = {
+		if(clock.getMinutesTo(time) > heatingTimeMin)"Liian vähän aikaa, aloitetaan lämmitys nyt, valmista on... "
+		else "Kelpaa, lämpenee valmiiksi kello " + time.toString
+	}
 
 	def toString = "uuni"
 }
