e9d38835631bbd43557e22b2750c0e65b13d5b0b
Aarn <pelto.karvanen@gmail.com>
2013-11-15 12:17:05 +0200

    DoorGuard asdf-versio valmis

--- SMSVahtimestari/src/smsVahtimestari/commands/DoorGuard.scala
+++ SMSVahtimestari/src/smsVahtimestari/commands/DoorGuard.scala
@@ -3,10 +3,19 @@ package smsVahtimestari.commands
 import smsVahtimestari.OnOffTrait
 
 object DoorGuard extends OnOffTrait {
+	var isOn = false
 
-	def turnOnOff(status: Boolean): String = ???
+	def turnOnOff(status: Boolean): String = {
+		if(isOn && status) "Ovivahti on jo päällä" 
+		else if(isOn) "Ovivahti sammutetaan" isOn = false 
+		else if(status)"Ovivahti päälle" isOn = true
+		else "Ovivahti ei ollut päällä" 
+	}
 
-	def status(): String = ???
+	def status(): String = {
+		if(isOn) "Ovivahti päällä"
+		else "Ovivahti pois päältä"
+	}
 
 	def toString = "ovivahti"
 }
