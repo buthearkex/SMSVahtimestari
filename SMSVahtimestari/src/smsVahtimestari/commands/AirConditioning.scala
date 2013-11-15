234f8b3217d6a4a9af82f09a3d81d3817b461c60
Aarn <pelto.karvanen@gmail.com>
2013-11-15 12:26:26 +0200

    Air conditioning asdf-versio

--- SMSVahtimestari/src/smsVahtimestari/commands/AirConditioning.scala
+++ SMSVahtimestari/src/smsVahtimestari/commands/AirConditioning.scala
@@ -5,17 +5,18 @@ import smsVahtimestari.OnOffTrait
 object AirConditioning extends OnOffTrait {
 
 	private var isOn = false
+	val currtentTemperature = 25
 
 	def status(): String = {
-		"asd"
-	}
+		val temp = "Asunnon lämpötila on nyt " + currtentTemperature.toString()
+		if(isOn) temp += " ja ilmastointi on päällä"
+		else temp += " ja ilmastointi ei ole päällä"
 
 	def turnOnOff(status: Boolean): String = {
-		"asd"
-	}
-
-	private def switch() = {
-
+		if(isOn && status) "Ilmastointi on jo päällä" 
+		else if(isOn) "Ilmastointi sammutetaan" isOn = false 
+		else if(status)"Ilmastointi lämmitetään" isOn = true
+		else "Ilmastointi ei ollut päällä" 
 	}
 	
 	def toString = "lämmitys"
