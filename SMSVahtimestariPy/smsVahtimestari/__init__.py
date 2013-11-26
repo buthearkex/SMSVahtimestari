7818d7ed05499d41297db39b101a8401c474addc
Aarn <pelto.karvanen@gmail.com>
2013-11-26 11:42:32 +0200

    Kommentoitu toimimattoman dialogin vanha pohja

--- SMSVahtimestariPy/smsVahtimestari/__init__.py
+++ SMSVahtimestariPy/smsVahtimestari/__init__.py
@@ -8,22 +8,32 @@ class CommandInterpreter:
         self.topic = ""
 
     def interpret(self, msg):
+        #prosessoi viestin osiin ja yhtenäistaa muotoilun
         wordList = msg.split(' ')
         for idx, word in enumerate(wordList):
             word.lower()
             word.strip()
             wordList[idx] = word
+
+        #jos viestissa ja kaskyissa on jokin sama niin muutetaan comentoa cutsuttavaa
+        #MUTTA EI SE SITÄ VOI TEHDÄ JOKA KERTA TAI MUUTEN EI TUU DIALOGIA
         commandToCall = None
         for cmd in SMSVahtimestari.commands:
             if (str(cmd) in wordList):
                 commandToCall = cmd
+
+        #jos asiasanaa ei loytynyt listasta
         if (commandToCall == None):
             return SMSVahtimestari.commands[0].status()
+        #kutsutaan kesken kaiken toista topikkia
         elif (self.topic != str(commandToCall)):
             self.topic = str(commandToCall)
             return commandToCall.status()
+
+        #varsinainen dialogi alkaa tasta
         else:
-            return "asd"
+            #tassa vaiheessa kaskee saunan lampenemaan jos paasee tanne asti
+            return "aasd" + Sauna().turnOnOff(True)
 
 class SMSVahtimestari:
     commands = [Help(), Sauna(), Oven(), Car(), AirConditioning(), Electricity(), DoorGuard()]
