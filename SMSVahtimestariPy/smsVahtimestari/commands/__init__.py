a0fb96513bcfe19527b8cb42687fa9ffd5e4c1a6
SieniMaagi <laurilavanti@gmail.com>
2013-11-29 15:45:01 +0200

    little fixes

--- SMSVahtimestariPy/smsVahtimestari/commands/__init__.py
+++ SMSVahtimestariPy/smsVahtimestari/commands/__init__.py
@@ -7,7 +7,7 @@ class Sauna:
         return 3
     
     def __init__(self):
-        self.heatingTimeMin = datetime.timedelta(minutes = 45)
+        self.heatingTimeMin = 45
         self.currentTemperature = 25
         self.currentTime = datetime.datetime.now()
         self.isOn = False
@@ -61,7 +61,7 @@ class Oven:
         return 3
     
     def __init__(self):
-        self.heatingTimeMin = datetime.timedelta(minutes = 45)
+        self.heatingTimeMin = 45
         self.currentTemperature = 25
         self.currentTime = datetime.datetime.now()
         self.isOn = False
@@ -113,7 +113,7 @@ class Car:
         return 2
     
     def __init__(self):
-        self.heatingTimeMin = datetime.timedelta(minutes=15)
+        self.heatingTimeMin = 15
         self.currentTime = datetime.datetime.now()
         self.isOn = False    
 
