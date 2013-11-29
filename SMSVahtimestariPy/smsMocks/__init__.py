0730cf7a351fa9a6bb8a901298acff68b26764f8
SieniMaagi <laurilavanti@gmail.com>
2013-11-29 17:15:11 +0200

    Refaktorointia

--- SMSVahtimestariPy/smsMocks/__init__.py
+++ SMSVahtimestariPy/smsMocks/__init__.py
@@ -1,5 +1,5 @@
 '''
-Created on 18.11.2013
+Contains mocks for receiving and sending text messages.
 
 @author: Leinonen
 '''
@@ -7,12 +7,25 @@ Created on 18.11.2013
 class SMSReceiver:
     
     def __init__(self, callback):
+        '''
+        Mock for receiving text messages.
+        Callback should be a function that can handle string-format messages.
+        '''
         self.callback = callback
     
     def listen(self):
+        '''
+        Starts listening for text messages and then sends the messages to callback given in init.
+        '''
         self.callback(input())
 
 class SMSSender:
+    '''
+    Mock for sending text messages.
+    '''
     
     def send(self, msg):
+        '''
+        Sends the finished message to user.
+        '''
         print(msg)
